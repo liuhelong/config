41705fef6fdceef6aa49e6b198cba47cc9af2192
liu <854583481@qq.com>
2020-05-20 17:38:50 +0800

    Update ConfigController.php

--- src/ConfigController.php
+++ src/ConfigController.php
@@ -76,20 +76,25 @@ class ConfigController
     {
         $grid = new Grid(new ConfigModel());
 
-        $grid->id('ID')->sortable();
-        $grid->name()->display(function ($name) {
+        $grid->column('id','ID')->sortable()->hide();
+        $grid->column('name','键值')->display(function ($name) {
             return "<a tabindex=\"0\" class=\"btn btn-xs btn-twitter\" role=\"button\" data-toggle=\"popover\" data-html=true title=\"Usage\" data-content=\"<code>config('$name');</code>\">$name</a>";
         });
-        $grid->value();
-        $grid->description();
 
-        $grid->created_at();
-        $grid->updated_at();
+        $grid->('value','值')->display(function ($value, $column) {
+			if ($this->type == 1) {
+				return $value;
+			}
+			return $column->image();
+		});
+        $grid->('description','描述');
+        $grid->('created_at','创建时间')->hide();
+        $grid->('updated_at','更新时间')->hide();
 
         $grid->filter(function ($filter) {
             $filter->disableIdFilter();
-            $filter->like('name');
-            $filter->like('value');
+            $filter->like('name','键值');
+            $filter->like('value','值');
         });
 
         return $grid;
@@ -99,17 +104,11 @@ class ConfigController
     {
         $form = new Form(new ConfigModel());
 
-        $form->display('id', 'ID');
-        $form->text('name')->rules('required');
-        if (config('admin.extensions.config.valueEmptyStringAllowed', false)) {
-            $form->textarea('value');
-        } else {
-            $form->textarea('value')->rules('required');
-        }
-        $form->textarea('description');
-
-        $form->display('created_at');
-        $form->display('updated_at');
+        $form->radio('type','类型')->options(['1'=>'文字','2'=>'图片'])->default(1)->required();
+		$form->text('name','键值')->required();
+		$form->textarea('value','值')->help('文字选填这个');;
+		$form->image('image','图片')->help('图片选填这个');
+		$form->textarea('description','描述');
 
         return $form;
     }
